f6b27d71d9536293351a05061b8f3fb59a1ccfbf
Ross Payne <rosshpayne@gmail.com>
2019-11-22 09:59:05 +1100

    working Query

---
+++ resolver/resolver.go
@@ -0,0 +1,67 @@
+package resolver
+
+import (
+	"fmt"
+	"strings"
+
+	sdl "github.com/graph-sdl/ast"
+)
+
+type fieldResolver interface {
+	TypeName() string
+	String() string
+}
+
+// Resolvers
+
+type ResolverFunc func(obj sdl.InputValueProvider, args sdl.ObjectVals) string
+
+type ResolverPath string
+
+type Resolvers struct {
+	resolverMap map[ResolverPath]ResolverFunc
+}
+
+func New() Resolvers {
+	return Resolvers{resolverMap: make(map[ResolverPath]ResolverFunc)}
+}
+
+func (r Resolvers) Register(path string, f ResolverFunc, override ...bool) error {
+
+	var pathField ResolverPath
+
+	pathField = ResolverPath(path)
+
+	if _, ok := r.resolverMap[pathField]; !ok {
+		fmt.Println("Register ", pathField)
+		r.resolverMap[pathField] = f
+		return nil
+	}
+	if len(override) > 0 && override[0] {
+		fmt.Println("Register ", pathField)
+		r.resolverMap[pathField] = f
+		return nil
+	}
+	return fmt.Errorf(`Resolver function already registered against path "%s"`, pathField)
+
+}
+
+func (r Resolvers) GetFunc(path string) ResolverFunc {
+
+	if f, ok := r.resolverMap[ResolverPath(path)]; ok {
+		return f
+	}
+	return nil
+
+}
+
+func (r Resolvers) String() string {
+	var s strings.Builder
+
+	for k := range r.resolverMap {
+		s.WriteString(string(k))
+		s.WriteString("\n")
+	}
+	return s.String()
+
+}
