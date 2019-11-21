862ccf5b3e7877d7155534353071879de2c5a537
Ross Payne <rosshpayne@gmail.com>
2019-11-22 09:39:29 +1100

    working Query

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,41 +1,39 @@
 package lexer
 
 import (
+	_ "fmt"
 	"unicode"
 	"unicode/utf8"
 
 	"github.com/graphql/token"
 )
 
-// pos preserves position of read position
-type readPos struct {
-	Line int
-	Col  int
-}
-
 // Lexer parses an Input string (embedded in token pkg) and returns it as tokens - defined in token package.
 type Lexer struct {
-	Eloc  readPos // position of illegal char
+	//	Eloc  token.Pos // Loc of illegal char
 	input string
-	cpos  int    // Current rune position in input
-	rpos  int    // Read rune position in input
+	cLoc  int    // Current rune Loc in input
+	rLoc  int    // Read rune Loc in input
 	ch    rune   // current rune under examination, added to token during lex processings
 	del   string // string delimeter
-	line  int
-	col   int // curren col position
+	Line  int
+	Col   int // curren col Loc
 	err   error
 }
 
-func (l *Lexer) Cpos() int {
-	return l.cpos
+func (l *Lexer) CLoc() int {
+	return l.cLoc
 }
 
 func (l *Lexer) Input() string {
 	return l.input
 }
 
+func (l *Lexer) Loc() (int, int) {
+	return l.Line, l.Col
+}
 func New(input string) *Lexer {
-	l := &Lexer{input: input, line: 1}
+	l := &Lexer{input: input, Line: 1}
 	l.readRune() // prime lexer struct
 	return l
 }
@@ -44,13 +42,13 @@ func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 	//	fmt.Printf("NextToken: %c\n", l.ch)
 	l.skipWhitespace() // scan to next non-whitespace and return its value as a token
-
+	//fmt.Printf("[%c]\n", l.ch)
 	switch l.ch {
 	case '\ufeff':
 		tok = l.newToken(token.BOM, l.ch)
 	case '#':
-		tok = l.newToken(token.COMMENT, l.ch)
 		l.readToEol()
+		return l.NextToken()
 	case '.': // ... expand sequence
 		if l.peekRune() == '.' {
 			//ch := l.ch
@@ -81,12 +79,17 @@ func (l *Lexer) NextToken() token.Token {
 			l.del = token.STRINGDEL
 			tok = l.readString()
 		}
+	case '!':
+		tok = l.newToken(token.BANG, l.ch)
 	case ':':
 		tok = l.newToken(token.COLON, l.ch)
-	case ',':
-		tok = l.newToken(token.COMMA, l.ch)
+	case '@':
+		tok = l.newToken(token.ATSIGN, l.ch)
+	// case ',':
+	// 	tok = l.newToken(token.COMMA, l.ch)
 	case '{':
 		tok = l.newToken(token.LBRACE, l.ch)
+		tok.Cat = token.VALUE
 	case '}':
 		tok = l.newToken(token.RBRACE, l.ch)
 	case '(':
@@ -109,8 +112,8 @@ func (l *Lexer) NextToken() token.Token {
 	default:
 		if unicode.IsLetter(l.ch) || l.ch == '_' {
 			tok = l.readIdentifier()
-			tok.Type, tok.Cat = token.LookupIdent(tok.Literal) // IDENT,nil or <keyword>,<VALUE | NONVALUE>
-		} else if unicode.IsDigit(l.ch) || l.ch == '-' || l.ch == '+' {
+			tok.Type, tok.Cat, tok.IsScalarType = token.LookupIdent(tok.Literal) // IDENT,nil or <keyword>,<VALUE | NONVALUE>
+		} else if unicode.IsDigit(l.ch) || l.ch == '-' {
 			tok = l.readNumber()
 		} else {
 			tok = l.newToken(token.ILLEGAL, l.ch)
@@ -126,11 +129,11 @@ func (l *Lexer) NextToken() token.Token {
 func (l *Lexer) skipWhitespace() {
 	// Horizontal Tab (U+0009) Space (U+0020)
 	// LineTerminator :: New Line (U+000A)
-	//  Carriage Return (U+000D) [lookahead ≠ New Line (U+000A)] Carriage Return (U+000D) New Line (U+000A)
+	//  Carriage Return (U+000D) [lookahead ≠ New Line (U+000A)] Carriage Return (U+000D) New Line (U+000A)
 	for l.ch == '\u0009' || l.ch == '\u0020' || l.ch == '\u000A' || l.ch == '\u000D' || l.ch == ',' {
 		if l.ch == '\n' { // linefeed
-			l.line++
-			l.col = 0
+			l.Line++
+			l.Col = 0
 		}
 		l.readRune()
 	}
@@ -138,45 +141,46 @@ func (l *Lexer) skipWhitespace() {
 
 func (l *Lexer) readRune() {
 	// get next byte in string
-	if l.rpos >= len(l.input) {
+	if l.rLoc >= len(l.input) {
 		l.ch = 0
+		l.cLoc++
 	} else {
 		var size int
 		// TODO: check token type. Only comment and string need rune reads all others simple ascii will suffice
-		l.ch, size = utf8.DecodeRuneInString(l.input[l.rpos:])
-		l.cpos = l.rpos
-		l.rpos += size
+		l.ch, size = utf8.DecodeRuneInString(l.input[l.rLoc:])
+		l.cLoc = l.rLoc
+		l.rLoc += size
 		if !(l.ch == '\n' || l.ch == '\r') {
-			l.col++
+			l.Col++
 		}
+		//	fmt.Printf("readRune: %c %d %d %d [%s]\n", l.ch, l.cLoc, l.rLoc, size, l.input[:l.rLoc])
 	}
-	//	fmt.Printf("readRune: %c\n", l.ch)
 
 }
 
 func (l *Lexer) peekRune() rune {
-	if l.rpos >= len(l.input) {
+	if l.rLoc >= len(l.input) {
 		return 0
 	} else {
-		rn, _ := utf8.DecodeRuneInString(l.input[l.rpos:])
+		rn, _ := utf8.DecodeRuneInString(l.input[l.rLoc:])
 		return rn
 	}
 }
 
 func (l *Lexer) readIdentifier() token.Token {
-	start := token.Pos{l.line, l.col}
-	pos := l.cpos
+	start := token.Pos{l.Line, l.Col}
+	Loc := l.cLoc
 	for unicode.IsLetter(l.ch) || l.ch == '_' || unicode.IsDigit(l.ch) {
 		l.readRune()
 	}
-	return token.Token{Cat: token.NONVALUE, Type: token.STRING, Literal: l.input[pos:l.cpos], Position: start}
+	return token.Token{Cat: token.NONVALUE, Type: token.STRING, Literal: l.input[Loc:l.cLoc], Loc: start}
 }
 
 func (l *Lexer) readNumber() token.Token {
 	var tokenT token.TokenType = token.INT
 	var illegalT bool
-	spos := l.cpos
-	start := token.Pos{l.line, l.col}
+	sLoc := l.cLoc
+	start := token.Pos{l.Line, l.Col}
 	if l.ch == '-' {
 		//l.skipWhitespace()
 		l.readRune()
@@ -214,7 +218,7 @@ func (l *Lexer) readNumber() token.Token {
 
 	default: // all letters other than e E
 		if unicode.IsLetter(l.ch) {
-			l.Eloc = readPos{l.line, l.col}
+			//l.Eloc = token.Pos{l.Line, l.Col}
 			// token is now interpreted as an illegal IDENT
 			illegalT = true
 			tokenT = token.IDENT
@@ -223,15 +227,22 @@ func (l *Lexer) readNumber() token.Token {
 			}
 		}
 	}
-	return token.Token{Cat: token.VALUE, Type: tokenT, Literal: l.input[spos:l.cpos], Illegal: illegalT, Position: start}
+	last := l.cLoc // no digits read after + or -
+	if sLoc == l.cLoc {
+		tokenT = token.IDENT
+		illegalT = true
+		last = l.cLoc + 2 // include rune next to + or -
+		l.readRune()      // read over + -
+	}
+	return token.Token{Cat: token.VALUE, Type: tokenT, Literal: l.input[sLoc:last], Illegal: illegalT, Loc: start}
 
 }
 
 func (l *Lexer) readString() token.Token {
 
-	pos := l.cpos + 1
-	start := token.Pos{l.line, l.col}
-	//fmt.Println("pos: ", pos)
+	Loc := l.cLoc + 1
+	start := token.Pos{l.Line, l.Col}
+	//fmt.Println("Loc: ", Loc)
 	for {
 		l.readRune()
 		if l.ch == '"' { // "
@@ -252,17 +263,17 @@ func (l *Lexer) readString() token.Token {
 			}
 		}
 		if l.del == token.RAWSTRINGDEL && (l.ch == 10) { // linefeed
-			l.line++
-			l.col = 0
+			l.Line++
+			l.Col = 0
 		}
 	}
-	//fmt.Println("l.cpos: ", l.cpos)
-	var epos int
+	//fmt.Println("l.cLoc: ", l.cLoc)
+	var eLoc int
 	if l.del == token.RAWSTRINGDEL {
-		epos = 2
-		return token.Token{Cat: token.VALUE, Type: token.RAWSTRING, Literal: l.input[pos : l.cpos-epos], Position: start}
+		eLoc = 2
+		return token.Token{Cat: token.VALUE, Type: token.RAWSTRING, Literal: l.input[Loc : l.cLoc-eLoc], Loc: start}
 	}
-	return token.Token{Cat: token.VALUE, Type: token.STRING, Literal: l.input[pos : l.cpos-epos], Position: start}
+	return token.Token{Cat: token.VALUE, Type: token.STRING, Literal: l.input[Loc : l.cLoc-eLoc], Loc: start}
 }
 
 func (l *Lexer) readToEol() {
@@ -275,9 +286,20 @@ func (l *Lexer) readToEol() {
 	}
 }
 
-func (l *Lexer) newToken(tokenType token.TokenType, ch rune, pos ...token.Pos) token.Token {
-	if len(pos) > 0 {
-		return token.Token{Cat: token.NONVALUE, Type: tokenType, Literal: string(ch), Position: pos[0]}
+func (l *Lexer) newToken(tokenType token.TokenType, ch rune, Loc ...token.Pos) token.Token {
+	if len(Loc) > 0 {
+		return token.Token{Cat: token.NONVALUE, Type: tokenType, Literal: string(ch), Loc: Loc[0]}
 	}
-	return token.Token{Cat: token.NONVALUE, Type: tokenType, Literal: string(ch), Position: token.Pos{l.line, l.col}}
+	return token.Token{Cat: token.NONVALUE, Type: tokenType, Literal: string(ch), Loc: token.Pos{l.Line, l.Col}}
 }
+
+// func (l *Lexer) GetLoc() *token.Pos {
+// 	return &token.Pos{l.Line, l.Col}
+// }
+
+// func (l *Lexer) SetELoc() {
+// 	l.Eloc = token.Pos{l.Line, l.Col}
+// }
+// func (l *Lexer) ClearLoc() {
+// 	l.Eloc = token.Pos{}
+// }
